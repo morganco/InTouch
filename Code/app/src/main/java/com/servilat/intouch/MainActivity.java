67b5c11c567088b9da63b86fb07bbdca75180089
Yauheni <bugatti1701@gmail.com>
2018-11-01 16:18:37 +0300

    Added string values and design for menu & main window

--- Code/app/src/main/java/com/servilat/intouch/MainActivity.java
+++ Code/app/src/main/java/com/servilat/intouch/MainActivity.java
@@ -1,4 +1,22 @@
 package com.servilat.intouch;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
 }
