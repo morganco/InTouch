69d0a2243fe339eb6bc6c624cab7109cc238cebb
Yauheni <bugatti1701@gmail.com>
2018-11-04 01:25:21 +0300

    Added read dialogs functionality and send messages

--- Code/app/src/main/java/com/servilat/intouch/MainActivity.java
+++ Code/app/src/main/java/com/servilat/intouch/MainActivity.java
@@ -1,22 +1,152 @@
 package com.servilat.intouch;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.servilat.intouch.fragment.DialogsListFragment;
+import com.servilat.intouch.login.LoginVKFragment;
+import com.squareup.picasso.Picasso;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKApiUserFull;
+import com.vk.sdk.api.model.VKList;
+
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    private Fragment visibleFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setBackgroundColor(getResources().getColor(R.color.colorVk));
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                if (visibleFragment != null) {
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, visibleFragment, "visible_fragment");
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                    fragmentTransaction.commit();
+                }
+            }
+        };
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
+
+        if (!VKSdk.isLoggedIn()) {
+            visibleFragment = new LoginVKFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.frame_layout, visibleFragment, "visible_fragment");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.commit();
+        } else {
+            showUserDialogs();
+            vkRequestForHeader();
+        }
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        visibleFragment = null;
+        switch (id) {
+            case R.id.nav_friends:
+                break;
+            case R.id.nav_messages:
+                if (!VKSdk.isLoggedIn()) {
+                    visibleFragment = new LoginVKFragment();
+                } else {
+                    showUserDialogs();
+                }
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    public void showUserDialogs() {
+        DialogsListFragment dialogsListFragment = new DialogsListFragment();
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, dialogsListFragment, "visible_dialogs");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
+    }
+
+    public void vkRequestForHeader() {
+        VKRequest vkRequest = VKApi.users().get(VKParameters.from(
+                VKApiConst.USER_IDS, VKAccessToken.currentToken().userId,
+                VKApiConst.FIELDS, "photo_100, status"));
+
+        vkRequest.executeWithListener(new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                super.onComplete(response);
+                VKApiUserFull currentVKUser = (VKApiUserFull) ((VKList) response.parsedModel).get(0);
+                String status = "";
+                try {
+                    status = currentVKUser.fields.getString("status");
+                } catch (JSONException ignored) {
+                }
+                fillNavigationDrawerHeader(currentVKUser.toString(), status, currentVKUser.photo_100);
+            }
+        });
+    }
+
+    private void fillNavigationDrawerHeader(String fullName, String status, String photo) {
+        ((TextView) findViewById(R.id.nav_user_name)).setText(fullName);
+        ((TextView) findViewById(R.id.nav_user_status)).setText(status);
+        Picasso.with(getApplicationContext())
+                .load(photo)
+                .placeholder(R.drawable.placeholder_person)
+                .error(R.drawable.placeholder_person)
+                .into((ImageView) findViewById(R.id.nav_user_photo));
     }
 }
